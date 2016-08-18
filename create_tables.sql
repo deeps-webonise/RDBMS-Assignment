9bccce1798574d7f5458cbdbdc8dc13395909b7b
deeps-webonise <deepn94@gmail.com>
2016-08-18 13:56:51 +0530

    Added tables.sql file

---
+++ create_tables.sql
@@ -0,0 +1,56 @@
+CREATE TABLE ecommerce.user_type(
+id int primary key not null,
+user_type text
+)
+
+CREATE TABLE ecommerce.Users(
+id int primary key not null,
+type_id int references ecommerce.user_type(id),
+text name,
+email varchar,
+pwd varchar
+)
+
+CREATE TABLE ecommerce.payment_source(
+id int primary key NOT NULL,
+type_payment text
+)
+
+CREATE TABLE ecommerce.orders(
+id int primary key not null,
+user_id int references ecommerce.users(id),
+cost real,
+payment_id int references ecommerce.payment_source(id)
+)
+
+CREATE TABLE ecommerce.product_color(
+id int primary key not null,
+color text
+)
+
+CREATE TABLE ecommerce.product(
+id int primary key  not null,
+color_id int references ecommerce.product_color(id),
+name text,
+price real
+)
+
+CREATE TABLE ecommerce.order_product(
+id int primary key not null,
+product_id int references ecommerce.product(id),
+order_id int references ecommerce.orders(id)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
