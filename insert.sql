9a7456b28b08466d460743a1811f63bd3da27b2a
deeps-webonise <deepn94@gmail.com>
2016-08-19 19:30:25 +0530

    Added trigger

--- insert.sql
+++ insert.sql
@@ -11,11 +11,14 @@ insert into ecommerce.payment_source values(1,'cash');
 insert into ecommerce.payment_source values(2,'card');
 insert into ecommerce.payment_source values(3,'coupons');
 
-insert into ecommerce.orders values(1,1,50,1);
-insert into ecommerce.orders values(2,1,100,1);
-insert into ecommerce.orders values(3,2,200,2);	
-insert into ecommerce.orders values(4,2,250,3);
-insert into ecommerce.orders values(5,3,300,1);
+--Initially the order cost will be 0
+--It will change after we enter values in order_product
+
+insert into ecommerce.orders values(1,1,0,1);
+insert into ecommerce.orders values(2,1,0,1);
+insert into ecommerce.orders values(3,2,0,2);	
+insert into ecommerce.orders values(4,2,0,3);
+insert into ecommerce.orders values(5,3,0,1);
 
 insert into ecommerce.product_color values(0,'only 1 color');
 insert into ecommerce.product_color values(1,'red');
@@ -26,11 +29,15 @@ insert into ecommerce.product values(2,'soap',50,2);
 insert into ecommerce.product values(3,'brush',10,0);
 insert into ecommerce.product values(4,'paste',20,0);
 
---When these values will be added a trigger will be created
+--When these values will be added a trigger will be created which will update the cost of the order
+
 insert into ecommerce.order_product values(1,1,1);
 insert into ecommerce.order_product values(2,2,1);
 insert into ecommerce.order_product values(3,3,2);
-insert into ecommerce.order_product values(4,4,2);
+insert into ecommerce.order_product values(4,3,2);
+insert into ecommerce.order_product values(5,3,3);
+insert into ecommerce.order_product values(6,2,4);
+insert into ecommerce.order_product values(7,1,4);
 
 
 
---
+++ trigger.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION ecommerce.upgradeRating() RETURNS TRIGGER AS '
+DECLARE
+amount integer ;
+sum integer;
+
+BEGIN
+select cost into sum from ecommerce.orders where id = NEW.order_id;
+select price into amount from ecommerce.product where id = NEW.product_id;
+sum:= sum + amount;
+update ecommerce.orders set cost = sum where id = NEW.order_id;
+RETURN NULL;
+END;
+' LANGUAGE plpgsql;
+
+
+CREATE TRIGGER update_price	
+AFTER INSERT ON ecommerce.order_product
+FOR EACH ROW
+EXECUTE PROCEDURE ecommerce.upgradeRating();
+
